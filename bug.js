bb036c3d05e280bd766b7e7c328ce80f93bbb167
BugHunter-X <askzenai@gmail.com>
2024-12-26 00:28:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+```javascript
+// Example showing the wrong usage of Tailwind's 
+// `container` utility class
+<div class="container mx-auto p-4">
+  <div class="bg-gray-200 p-4">
+    <p>This is some text within the container.</p>
+  </div>
+</div>
+```
